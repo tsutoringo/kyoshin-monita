0ca17adf794b871a6c7e74e8fbec4ffdd8ee0486
tsutoringo <33830878+tsutoringo@users.noreply.github.com>
2021-02-22 17:27:20 +0900

    Added Latest structure

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ module.exports.Client = require('./lib/Client');
 
 module.exports.EEW = require('./lib/structures/EEW');
 module.exports.Report = require('./lib/structures/Report');
+module.exports.Latest = require('./lib/structures/Latest');
 module.exports.Security = require('./lib/structures/Security');
