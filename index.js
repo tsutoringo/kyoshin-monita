6b048bebb85ab61f073e33e545ff6b2c497ec62e
tsutoringo <33830878+tsutoringo@users.noreply.github.com>
2021-02-22 11:44:52 +0900

    added Security

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
+module.exports.Client = require('./lib/Client');
+
 module.exports.EEW = require('./lib/structures/EEW');
 module.exports.Report = require('./lib/structures/Report');
-
-module.exports.Client = require('./lib/Client');
+module.exports.Security = require('./lib/structures/Security');
